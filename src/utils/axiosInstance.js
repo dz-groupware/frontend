48dbd25551cd723c0d38cdc9d8ba8d53f46f73f7
subo <kws3363@gmail.com>
2023-09-29 15:07:06 +0900

    [test]: 배포확인을 위한 uri변경

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8010/api/v1",
+  baseURL: "http://dev.amaranth2023.site/api/v1",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
   timeout: 10000,

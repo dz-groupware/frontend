5f6ce7f427fbcf0f7eb3287f37d27995efad7301
subo <kws3363@gmail.com>
2023-09-30 16:05:40 +0900

    [chore]: 머지전 변경사항 저장

--- src/api/login.js
+++ src/api/login.js
@@ -9,6 +9,7 @@ export const loginApi = async ({ data }) => {
     console.log(response);
     return response;
   } catch (error) {
+    console.error(error)
     console.error('로그인 실패');
     throw error; // 오류를 throw하여 상위로 전달합니다.
   }
